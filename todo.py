1034e89427772b98a9ba9356302acf5a02325f6f
EfecanEfe <mh.efecanefe@gmail.com>
2022-07-07 19:04:05 +0300

    Updated todo.py

--- todo.py
+++ todo.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////Users/efeca/Desktop/python_projects/todo-app/todo.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////Users/efeca/Desktop/python_projects/todo-app-flask/todo.db"
 
 db = SQLAlchemy(app)
 
